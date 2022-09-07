e1b1aa4ac0d3840b3083921a28b23cef4f2d24e0
francho96 <71195484+francho96@users.noreply.github.com>
2022-09-07 01:49:22 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition, Popover } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import { render } from "react-dom";
+import App from "./App";
+import Trabajos from './pages/trabajos';
+import Contacto from './pages/contacto';
+
+import './index.css';
+
+
+import index  from './pages/img/index2.png'
+
+
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="trabajos" element={<Trabajos />} />
+      <Route path="contacto" element={<Contacto />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
